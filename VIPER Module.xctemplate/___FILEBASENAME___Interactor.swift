9cc8ca317b14ecbd78389f9605ff7dbc1eed7664
Nikita Kuzmin <Dr.Cappo@gmail.com>
2022-02-22 10:41:59 +0300

    Update ___FILEBASENAME___Interactor.swift

--- VIPER Module.xctemplate/___FILEBASENAME___Interactor.swift
+++ VIPER Module.xctemplate/___FILEBASENAME___Interactor.swift
@@ -11,11 +11,11 @@ import Foundation
 protocol ___VARIABLE_ModuleName___InteractorProtocol: AnyObject {}
 
 final class ___VARIABLE_ModuleName___Interactor {
-    // MARK: - Properties
-    weak var output: ___VARIABLE_ModuleName___InteractorOutput!
+    // MARK: - Private Properties
+    private weak var output: ___VARIABLE_ModuleName___InteractorOutput!
 }
 
 // MARK: - ___VARIABLE_ModuleName___InteractorProtocol
-extension ___VARIABLE_ModuleName___Interactor: ___VARIABLE_ModuleName___InteractorProtocol {
+private extension ___VARIABLE_ModuleName___Interactor: ___VARIABLE_ModuleName___InteractorProtocol {
     // TODO: - Implement Interactor Methods
 }
