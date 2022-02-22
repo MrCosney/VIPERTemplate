ea841160f2c8e8f4946722e5a9f41dcb01fc04cc
Nikita Kuzmin <Dr.Cappo@gmail.com>
2022-02-22 10:42:58 +0300

    Update ___FILEBASENAME___Router.swift

--- VIPER Module.xctemplate/___FILEBASENAME___Router.swift
+++ VIPER Module.xctemplate/___FILEBASENAME___Router.swift
@@ -12,10 +12,10 @@ protocol ___VARIABLE_ModuleName___PresenterOutput {}
 
 final class ___VARIABLE_ModuleName___Router {
     // MARK: - External dependency
-    weak var moduleOutput: ___VARIABLE_ModuleName___ModuleOutput?
+    private weak var moduleOutput: ___VARIABLE_ModuleName___ModuleOutput?
 }
 
 // MARK: - ___VARIABLE_ModuleName___PresenterOutput
-extension ___VARIABLE_ModuleName___Router: ___VARIABLE_ModuleName___PresenterOutput {
+private extension ___VARIABLE_ModuleName___Router: ___VARIABLE_ModuleName___PresenterOutput {
     // TODO: - Implement Interactor Outputs Methods
 }
