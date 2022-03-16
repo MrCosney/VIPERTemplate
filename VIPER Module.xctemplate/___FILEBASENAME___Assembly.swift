a40d6ae9084009140995bbc6ddcfe009c567bfcf
Nikita Kuzmin <Dr.Cappo@gmail.com>
2022-03-16 14:23:39 +0300

    Update ___FILEBASENAME___Assembly.swift

--- VIPER Module.xctemplate/___FILEBASENAME___Assembly.swift
+++ VIPER Module.xctemplate/___FILEBASENAME___Assembly.swift
@@ -10,6 +10,7 @@ final class ___VARIABLE_ModuleName___Assembly {
     typealias PresenterInput = ___VARIABLE_ModuleName___ViewControllerOutput & ___VARIABLE_ModuleName___InteractorOutput
 
     // MARK: - Static method to build ___VARIABLE_ModuleName___ VIPER Module
+
     static func buildModule(moduleOutput: ___VARIABLE_CoordinatorName___Protocol? = nil) -> ___VARIABLE_ModuleName___ViewController {
         let viewController = ___VARIABLE_ModuleName___ViewController()
         let interactor = ___VARIABLE_ModuleName___Interactor()
