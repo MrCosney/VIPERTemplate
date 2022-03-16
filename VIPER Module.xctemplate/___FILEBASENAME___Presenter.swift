eb6a309fd8c48bc5f920d9c0d2e4667adc422d3f
Nikita Kuzmin <Dr.Cappo@gmail.com>
2022-03-16 14:24:23 +0300

    Update ___FILEBASENAME___Presenter.swift

--- VIPER Module.xctemplate/___FILEBASENAME___Presenter.swift
+++ VIPER Module.xctemplate/___FILEBASENAME___Presenter.swift
@@ -13,12 +13,15 @@ protocol ___VARIABLE_ModuleName___ViewControllerOutput: AnyObject {}
 protocol ___VARIABLE_ModuleName___InteractorOutput: AnyObject {}
 
 final class ___VARIABLE_ModuleName___Presenter {
+
     // MARK: - External dependencies
+
     private weak var view: ___VARIABLE_ModuleName___ViewControllerInput?
     private var interactor: ___VARIABLE_ModuleName___InteractorProtocol
     private var moduleOutput: ___VARIABLE_CoordinatorName___Protocol?
 
     // MARK: - Initialisation
+    
     init(
         view: ___VARIABLE_ModuleName___ViewControllerInput,
         interactor: ___VARIABLE_ModuleName___InteractorProtocol,
@@ -31,14 +34,17 @@ final class ___VARIABLE_ModuleName___Presenter {
 }
 
 // MARK: - Extension for the huge methods of ___VARIABLE_ModuleName___Presenter
+
 private extension ___VARIABLE_ModuleName___Presenter {}
 
 // MARK: - ___VARIABLE_ModuleName___ViewControllerOutput
+
 extension ___VARIABLE_ModuleName___Presenter: ___VARIABLE_ModuleName___ViewControllerOutput {
     // TODO: - Implement View Output Methods
 }
 
 // MARK: - ___VARIABLE_ModuleName___InteractorOutput
+
 extension ___VARIABLE_ModuleName___Presenter: ___VARIABLE_ModuleName___InteractorOutput {
     // TODO: - Implement Interactor Outputs Methods
 }
