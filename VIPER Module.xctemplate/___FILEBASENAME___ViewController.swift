8cc8218aa17274a85fafbcbf66efe7b86547a5a2
Nikita Kuzmin <Dr.Cappo@gmail.com>
2022-03-16 14:24:41 +0300

    Update ___FILEBASENAME___ViewController.swift

--- VIPER Module.xctemplate/___FILEBASENAME___ViewController.swift
+++ VIPER Module.xctemplate/___FILEBASENAME___ViewController.swift
@@ -11,21 +11,24 @@ import UIKit
 protocol ___VARIABLE_ModuleName___ViewControllerInput: AnyObject {}
 
 final class ___VARIABLE_ModuleName___ViewController: UIViewController {
+    
     // MARK: - Public Properties
+   
     var output: ___VARIABLE_ModuleName___ViewControllerOutput?
 
     // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    // MARK: - Methods
 }
 
 // MARK: - Extension for the huge methods of ___VARIABLE_ModuleName___ViewController
+
 private extension ___VARIABLE_ModuleName___ViewController {}
 
 // MARK: - ___VARIABLE_ModuleName___ViewControllerInput
+
 extension ___VARIABLE_ModuleName___ViewController: ___VARIABLE_ModuleName___ViewControllerInput {
     // TODO: - Implement View Input Methods
 }
